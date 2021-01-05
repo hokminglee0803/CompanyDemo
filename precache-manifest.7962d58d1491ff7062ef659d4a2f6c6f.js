14cad3509a63357e14c962dc723265183b72aae5
Marco HM Lee <marco.hm.lee@prudential.com.hk>
2021-01-06 02:10:03 +0800

    Initial Commit

---
+++ precache-manifest.7962d58d1491ff7062ef659d4a2f6c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e434e48497efda1a3fae52b6f24fab63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d978e57709fe6c742dba",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "./static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d978e57709fe6c742dba",
+    "url": "./static/js/main.8a405f19.chunk.js"
+  },
+  {
+    "revision": "bb30c7d0fbfa6286a3a5",
+    "url": "./static/js/runtime-main.4e496788.js"
+  }
+]);
